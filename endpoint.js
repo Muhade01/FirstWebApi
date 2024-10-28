f798e63b3775e827fd34302f183d1e1ad6ab7791
Muhammad Adeel <muhammadadeel@Muhammads-MacBook-Air.local>
2024-10-28 12:15:07 +0100

    Add endpoint function to server.js

---
+++ endpoint.js
@@ -0,0 +1,6 @@
+// endpoint.js
+function endpoint() {
+    return "Hello from endpoint";
+}
+
+module.exports = endpoint; // Ensure you are exporting the function
--- server.js
+++ server.js
@@ -1,5 +1,12 @@
+// server.js
+const endpoint = require('./endpoint'); // Import the endpoint function
+
 function main() {
-    console.log("Hello, world");
+    console.log("hello world");
 }
 
+// Call the main function
 main();
+
+// Call the endpoint function and log the result
+console.log(endpoint());
